b9cba281dbc928499afc511c7fc701c573263272
Mayank Verma <mayankverma.dev@gmail.com>
2026-05-18 14:29:09 +0000

    new supabase

--- src/components/Supabase/SupabaseClient.js
+++ src/components/Supabase/SupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rwpwbgnnxbdlhuswbevu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3cHdiZ25ueGJkbGh1c3diZXZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMzkyNzAsImV4cCI6MjA2NDgxNTI3MH0.NgDrmxtSOw4vJFTuK9zYx8VBOCTtB0_ZQYTNgsx_s-s';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
